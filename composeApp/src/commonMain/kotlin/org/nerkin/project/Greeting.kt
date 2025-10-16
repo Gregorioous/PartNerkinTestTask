8730ef04c322dc0819b61b2535269caa98cb171b
EgorKholod <veh@kvartel.com>
2025-10-16 22:36:58 +0300

    initial_commit

---
+++ composeApp/src/commonMain/kotlin/org/nerkin/project/Greeting.kt
@@ -0,0 +1,9 @@
+package org.nerkin.project
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/org/nerkin/project/MainViewController.kt
@@ -0,0 +1,5 @@
+package org.nerkin.project
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
